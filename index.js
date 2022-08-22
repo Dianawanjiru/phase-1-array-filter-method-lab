746a18a46bd89ce331d998d554aead1978cc6113
Diana <diana.wanjiru@student.moringaschool.com>
2022-08-22 05:22:35 +0300

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+let drivers = ["Bobby", "Sammy","Annette", "Sarah", "Bobby"];
+
+  function findMatching (drivers, searchName) {
+    return drivers.filter((driver)=>{
+        return driver.toLowerCase().includes(searchName.toLowerCase())
+    });
+
+  
+  }
+
+  function fuzzyMatch (drivers, searchString){
+    return drivers.filter((matchNames)=>{
+        return matchNames.toLowerCase().startsWith(searchString.toLowerCase())
+    });
+  }
+
+  function matchName (driverObj, nameInput){
+    return driverObj.filter((driverNames)=>{
+        return driverNames.name === nameInput
+    });
+  }
+  
